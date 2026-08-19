7973d40cb11aaf161d62749e196bec7a39ff8b86
habin08090 <gkqls0809@gmail.com>
2026-08-19 15:13:13 +0900

    fix: LlmFeignConfig Retryer/Options 제거로 빈 충돌 해결

--- src/main/java/com/sprint/mission/otboo/external/llm/LlmFeignConfig.java
+++ src/main/java/com/sprint/mission/otboo/external/llm/LlmFeignConfig.java
@@ -1,23 +1,10 @@
 package com.sprint.mission.otboo.external.llm;
 
-import feign.Request;
 import feign.RequestInterceptor;
-import feign.Retryer;
-import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class LlmFeignConfig {
-
-  @Bean
-  public Request.Options llmRequestOptions() {
-    return new Request.Options(5, TimeUnit.SECONDS, 30, TimeUnit.SECONDS, true);
-  }
-
-  @Bean
-  public Retryer llmRetryer() {
-    return Retryer.NEVER_RETRY;
-  }
 
   @Bean
   public RequestInterceptor llmAuthInterceptor(
